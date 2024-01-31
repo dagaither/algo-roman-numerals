a131a065d1f5f5221a8975d26a7890dc6f227de7
David Gaither <gaither@protonmail.com>
2024-01-31 17:19:38 -0500

    Roman numerals files

--- romanNumerals.js
+++ romanNumerals.js
@@ -1,9 +1,54 @@
 function toRomanLazy(num) {
-  return "";
+  let output = ""
+  let romanNumeralToArabic = { 
+    M: 1000, D: 500, C: 100, 
+    L: 50, X: 10, V: 5, I: 1
+  };
+
+  let romanNumeralPriorityOrder = ["M", "D", "C", "L", "X", "V", "I"]
+  
+  let i = 0
+
+  while (i < romanNumeralPriorityOrder.length) {
+      let qty = Math.floor(num / romanNumeralToArabic[romanNumeralPriorityOrder[i]]);
+      while (qty > 0) {
+        output = output + romanNumeralPriorityOrder[i]
+        num -= romanNumeralToArabic[romanNumeralPriorityOrder[i]]
+        qty -= 1
+      }
+    i++;
+  }
+  return output
 }
 
+
 function toRoman(num) {
-  return "";
+  let output = ""
+  
+  let romanNumeralToArabic = { 
+    M: 1000, CM: 900, D: 500, CD: 400, 
+    C: 100,  XC: 90, L: 50, XL: 40, X: 10, 
+    IX: 9, V: 5, IV: 4, I: 1
+  };
+  
+  let romanNumeralPriorityOrder = [
+    "M", "CM", "D", "CD", 
+    "C", "XC", "L", "XL", "X", 
+    "IX", "V", "IV", "I"
+  ]
+  
+  let i = 0
+  
+  while (i < romanNumeralPriorityOrder.length) {
+      let qty = Math.floor(num / romanNumeralToArabic[romanNumeralPriorityOrder[i]]);
+      while (qty > 0) {
+        output = output + romanNumeralPriorityOrder[i]
+        num -= romanNumeralToArabic[romanNumeralPriorityOrder[i]]
+        qty -= 1
+      }
+    i++;
+  }
+  return output
 }
 
 module.exports = { toRoman, toRomanLazy };
