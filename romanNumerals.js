4f8e42fe38ae32f5f10c1c45392d4a60bd48f1c5
David Gaither <gaither@protonmail.com>
2024-01-31 19:01:13 -0500

    Re-formatted for readability and best practices

--- romanNumerals.js
+++ romanNumerals.js
@@ -1,54 +1,57 @@
 function toRomanLazy(num) {
-  let output = ""
-  let romanNumeralToArabic = { 
+
+  let output = "";
+
+  const romanNumeralToArabic = { 
     M: 1000, D: 500, C: 100, 
     L: 50, X: 10, V: 5, I: 1
   };
 
-  let romanNumeralPriorityOrder = ["M", "D", "C", "L", "X", "V", "I"]
+  const romanNumeralPriorityOrder = ["M", "D", "C", "L", "X", "V", "I"];
   
-  let i = 0
+  let i = 0;
 
   while (i < romanNumeralPriorityOrder.length) {
       let qty = Math.floor(num / romanNumeralToArabic[romanNumeralPriorityOrder[i]]);
       while (qty > 0) {
-        output = output + romanNumeralPriorityOrder[i]
-        num -= romanNumeralToArabic[romanNumeralPriorityOrder[i]]
-        qty -= 1
-      }
+        output = output + romanNumeralPriorityOrder[i];
+        num -= romanNumeralToArabic[romanNumeralPriorityOrder[i]];
+        qty -= 1;
+      };
     i++;
-  }
-  return output
-}
+  };
+  return output;
+};
 
 
 function toRoman(num) {
-  let output = ""
   
-  let romanNumeralToArabic = { 
+  let output = "";
+  
+  const romanNumeralToArabic = { 
     M: 1000, CM: 900, D: 500, CD: 400, 
     C: 100,  XC: 90, L: 50, XL: 40, X: 10, 
     IX: 9, V: 5, IV: 4, I: 1
   };
   
-  let romanNumeralPriorityOrder = [
+  const romanNumeralPriorityOrder = [
     "M", "CM", "D", "CD", 
     "C", "XC", "L", "XL", "X", 
     "IX", "V", "IV", "I"
-  ]
+  ];
   
-  let i = 0
+  let i = 0;
   
   while (i < romanNumeralPriorityOrder.length) {
       let qty = Math.floor(num / romanNumeralToArabic[romanNumeralPriorityOrder[i]]);
       while (qty > 0) {
-        output = output + romanNumeralPriorityOrder[i]
-        num -= romanNumeralToArabic[romanNumeralPriorityOrder[i]]
-        qty -= 1
-      }
+        output = output + romanNumeralPriorityOrder[i];
+        num -= romanNumeralToArabic[romanNumeralPriorityOrder[i]];
+        qty -= 1;
+      };
     i++;
-  }
-  return output
-}
+  };
+  return output;
+};
 
 module.exports = { toRoman, toRomanLazy };
